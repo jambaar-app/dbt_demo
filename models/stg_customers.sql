7181efb3df1f964c4519b9e70ab4ab2cd8d948b1
atmunyengango@gmail.com <atmunyengango@gmail.com>
2023-09-14 02:16:20 +0000

    updates

---
+++ models/stg_customers.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+
+with customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from raw.jaffle_shop.customers
+
+)
+select * from customers
